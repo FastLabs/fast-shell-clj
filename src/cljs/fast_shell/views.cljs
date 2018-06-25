e252f1d24bc153ece20fc1624ca7adb6fc1e7443
Oleg Bulavitchi <bulavitchi@gmail.com>
2018-06-25 21:27:15 +0100

    worked on application launcher

--- src/cljs/fast_shell/views.cljs
+++ src/cljs/fast_shell/views.cljs
@@ -3,7 +3,7 @@
             [re-com.core :refer [v-box modal-panel]]
             [fast-shell.subs :as subs]
             [reagent.core :as re]
-            [openfin.core :as of]
+          ;  [openfin.core :as of]
             [element.app-menu :as app-menu]))
 
 (def sessions [{}])
@@ -28,20 +28,20 @@
    [:div.level [:nav.level-left [session-bar sessions]]
     [:div.level-item [app-canvas]]]])
 
-(defn app-fin-test []
-  [:div
-   [:button {:on-click #(of/version)} "OpenFinn Version"]
-   [:button {:on-click #(of/dev-tools)} "Dev tools"]])
+'(defn app-fin-test []
+   [:div
+    [:button {:on-click #(of/version)} "OpenFinn Version"]
+    [:button {:on-click #(of/dev-tools)} "Dev tools"]])
 
 (defn app-modal [show-app]
   (prn "first app")
   (when @show-app
-    [modal-panel  :child [app-menu/app-list]]))
+    [modal-panel :child [app-menu/app-list show-app]]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])
         show-app (re/atom false)]
-    [:div [app-menu/app-menu show-app]
-     [app-modal show-app]]))
+     [:div [app-menu/app-menu show-app]
+      [app-modal show-app]]))
 ;[app-fin-test]]))
 
